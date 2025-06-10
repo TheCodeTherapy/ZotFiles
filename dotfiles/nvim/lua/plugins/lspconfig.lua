d128c8c3991e1776175d9c77d366c2914b9c3c47
TheCodeTherapy <me@mgz.me>
2025-06-10 18:36:28 +0100

    updates nvim settings

--- dotfiles/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/plugins/lspconfig.lua
@@ -120,6 +120,9 @@ return {
           },
         },
       },
+      tsserver = {
+        root_dir = require("lspconfig.util").root_pattern("lerna.json", ".git"),
+      },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
