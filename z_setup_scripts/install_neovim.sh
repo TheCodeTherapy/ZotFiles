99099aaa3acbf6d5b38e03b5285692f7c7201493
TheCodeTherapy <me@mgz.me>
2025-09-05 12:09:12 +0100

    updates neovim

--- z_setup_scripts/install_neovim.sh
+++ z_setup_scripts/install_neovim.sh
@@ -5,10 +5,10 @@ source "$SCRIPT_DIR/_helpers.sh"
 source "$SCRIPT_DIR/_config.sh"
 
 install_neovim() {
-  if /usr/bin/nvim --version | grep "v0.10.1" >/dev/null 2>&1; then
-    print_info "Neovim v0.10.1 is already installed ..."
+  if /usr/bin/nvim --version | grep "v0.11.4" >/dev/null 2>&1; then
+    print_info "Neovim v0.11.4 is already installed ..."
   else
-    print_info "Installing Neovim v0.10.1 ..."
+    print_info "Installing Neovim v0.11.4 ..."
 
     sudo apt-get remove --purge -y -qq neovim || handle_error "Failed to remove neovim."
     sudo apt-get autoremove -y -qq || handle_error "Autoremove failed."
@@ -30,8 +30,8 @@ install_neovim() {
     git fetch --tags --quiet ||
       handle_error "Failed to fetch tags"
 
-    git checkout v0.10.1 -q ||
-      handle_error "Failed to checkout version v0.10.1"
+    git checkout v0.11.4 -q ||
+      handle_error "Failed to checkout version v0.11.4"
 
     make CMAKE_BUILD_TYPE=RelWithDebInfo -j"$(nproc)" ||
       handle_error "Failed to build Neovim"
@@ -41,7 +41,7 @@ install_neovim() {
     cpack -G DEB ||
       handle_error "Failed to create Neovim package"
 
-    sudo dpkg -i --force-all nvim-linux64.deb ||
+    sudo dpkg -i --force-all nvim-linux-x86_64.deb ||
       sudo DEBIAN_FRONTEND=noninteractive apt-get install -f -y -qq ||
       handle_error "Failed to install Neovim package"
 
